b352af05aae3e0edf018b66e7b04390b98485043
peteral <ladislav.petera@yahoo.de>
2017-11-01 20:45:48 +0100

    made it runnable

--- handwritten-recognition/src/test.py
+++ handwritten-recognition/src/test.py
@@ -3,6 +3,7 @@ import network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
+# 784 input neurons, 30 hidden neurons, 10 output neurons
 net = network.Network([784, 30, 10])
 
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
